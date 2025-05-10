7c1057ad94c481bbc8449d945bafd99b181a9307
John <yohannes.h93@gmail.com>
2025-05-10 18:51:15 +0300

    creating responsive layout with chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { Accordion, Span } from "@chakra-ui/react";
-
-const items = [
-  { value: "a", title: "First Item", text: "Some value 1..." },
-  { value: "b", title: "Second Item", text: "Some value 2..." },
-  { value: "c", title: "Third Item", text: "Some value 3..." },
-];
+import { Grid, GridItem, HStack } from "@chakra-ui/react";
+import GameGrid from "./components/GameGrid";
 
 const App = () => {
   return (
     <>
-      <Accordion.Root collapsible defaultValue={["a"]}>
-        {items.map((item, index) => (
-          <Accordion.Item key={index} value={item.value}>
-            <Accordion.ItemTrigger>
-              <Span flex="1">{item.title}</Span>
-              <Accordion.ItemIndicator />
-            </Accordion.ItemTrigger>
-            <Accordion.ItemContent>
-              <Accordion.ItemBody>{item.text}</Accordion.ItemBody>
-            </Accordion.ItemContent>
-          </Accordion.Item>
-        ))}
-      </Accordion.Root>
+      <HStack>nav bar</HStack>
+      <Grid gridTemplateColumns={`repeat(6, 1fr)`}>
+        <GridItem
+          bg={"dodgerblue"}
+          as="aside"
+          colSpan={{ base: 1, md: 1, lg: 1 }}
+          h="100vh"
+        >
+          aside
+        </GridItem>
+        <GridItem
+          bg="purple.400"
+          as="main"
+          colSpan={{ base: 5, md: 5, lg: 5 }}
+          p="20px"
+        >
+          <GameGrid />
+        </GridItem>
+      </Grid>
     </>
   );
 };
---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,23 @@
+import { Box, SimpleGrid } from "@chakra-ui/react";
+
+function GameGrid() {
+  return (
+    <SimpleGrid minChildWidth="200px" textAlign="center" gap="20px">
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+      <Box border="2px blue solid">box</Box>
+    </SimpleGrid>
+  );
+}
+
+export default GameGrid;
