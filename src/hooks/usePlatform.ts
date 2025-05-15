35f5d52c4579d364bb9751c9a80e1366c14229f1
John <yohannes.h93@gmail.com>
2025-05-16 01:17:17 +0300

    refactor

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,11 +1,5 @@
 import platforms from "@/Data/platforms";
 
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-
 const usePlatform = () => ({ data: platforms, isLoading: false, error: false });
 
 export default usePlatform;
